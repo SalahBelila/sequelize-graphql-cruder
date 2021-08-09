d6700eb98ab005be0a689edf8aa1264e205e605b
Salah Belila <salahbelila@gmail.com>
2021-08-09 01:11:15 +0100

    Move custom-scalars -> ./src/custom-scalars

--- custom-scalars/index.js
+++
@@ -1,8 +0,0 @@
-const DateScalar = require('./Date');
-const TimeScalar = require('./Time');
-const NumericScalar = require('./Numeric');
-module.exports = {
-    DateScalar,
-    TimeScalar,
-    NumericScalar,
-}
