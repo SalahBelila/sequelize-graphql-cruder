9dfaa11ca160964ec844a17ff4e8a83e47efd2cf
Salah Belila <salahbelila@gmail.com>
2021-08-09 02:32:55 +0100

    Expose the BigInt custom scalar

--- src/custom-scalars/index.js
+++ src/custom-scalars/index.js
@@ -1,6 +1,7 @@
 const DateScalar = require('./Date');
 const TimeScalar = require('./Time');
 const NumericScalar = require('./Numeric');
+const BigIntScalar = require('./BigInt');
 module.exports = {
     DateScalar,
     TimeScalar,
